b5d1070928330b2360b7910ad7af803affc1a2a8
gaotianye <15156506755@163.com>
2017-06-07 22:03:10 +0800

    add：测试代码

---
+++ src/test/java/cn/celloud/bf/stormProject/Test.java
@@ -0,0 +1,16 @@
+package cn.celloud.bf.stormProject;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+import cn.celloud.bf.stormProject1.utils.CaculateUtils;
+
+public class Test {
+	public static void main(String[] args) {
+		Double a = 123.6;
+		Double b = 1.6;
+		System.out.println(a+b);
+		System.out.println(a*b);
+	}
+}
